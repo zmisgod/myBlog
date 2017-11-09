38024435c7c169aa57d419c008cfc7952445ed5a
zmisgod <starzmisgod@gmail.com>
2017-11-09 23:55:02 +0800

    new

--- myblog/src/store/blog/actions.js
+++ myblog/src/store/blog/actions.js
@@ -1,13 +1,14 @@
 export const actions = {
     showIndexArticle({ commit, state }) {
-        axios.get('http://127.0.0.1:8081/v1/home?page=' + state.columnId[state.nowColumn]).then(res => {
-            if (res.data.code === 200) {
+        console.log()
+        axios.get(process.env.API_HOST + '/v1/home?page=' + state.columnId[state.nowColumn]).then(res => {   
+        if (res.data.code === 200) {
                 commit('ARTICLELISTS', res.data.data)
             }
         })
     },
     showArticle({ commit, state }) {
-        axios.get('http://127.0.0.1:8081/v1/home/' + state.columnId[state.nowColumn]).then(res => {
+        axios.get(process.env.API_HOST + '/v1/home/' + state.columnId[state.nowColumn]).then(res => {
             if (res.data.code === 200) {
                 commit('ARTICLEOBJECT', res.data.data)
             }
--- myblog/src/store/index.js
+++ myblog/src/store/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import blog from './blog'
-
 Vue.use(Vuex)
 
 export default new Vuex.Store({
